4acfc1f0699bdc31addc854abfc7e1aa2612527e
Work <79774488441@ya.ru>
2022-09-06 19:27:46 +0300

    jackson work well

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -8,9 +8,10 @@ import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
 
 import java.security.Principal;
+import java.util.List;
 
-@Controller
-@RequestMapping("/admin")
+@RestController
+@RequestMapping("/admin/api")
 public class AdminController {
 
     private final UserService userService;
@@ -20,50 +21,56 @@ public class AdminController {
         this.userService = userService;
     }
 
-    @GetMapping
-    public String main(Model model, Principal principal) {
-        model.addAttribute("user", userService.findByUsername(principal.getName()));
-        model.addAttribute("users",userService.getAll());
-        model.addAttribute("listRoles", userService.listRoles());
-        return "admin";
+    @GetMapping("/users")
+    public List<User> showAllUsers() {
+        List<User> allUsers = userService.getAll();
+        return allUsers;
     }
 
-    @GetMapping("/search")
-    public String searchUser(@RequestParam(value = "keyword") String keyword, Model model) {
-        model.addAttribute("users", userService.search(keyword));
-        return "searchUser";
-    }
-
-    @GetMapping("/newUser")
-    public String newUser(Model model) {
-        model.addAttribute("user", new User());
-        model.addAttribute("listRoles", userService.listRoles());
-        return "newUser";
-    }
-
-    @PostMapping()
-    public String createUser(@ModelAttribute("user") User user) {
-        userService.save(user);
-        return "redirect:/admin";
-    }
-
-    @GetMapping("/{id}/edit")
-    public String editUser(@PathVariable("id") long id, Model model) {
-        model.addAttribute("user", userService.get(id));
-        model.addAttribute("listRoles", userService.listRoles());
-        return "editUser";
-    }
-
-    @PatchMapping("/{id}")
-    public String updateUser(@ModelAttribute("user") User user, @PathVariable("id") long id) {
-        userService.update(user);
-        return "redirect:/admin";
-    }
-
-    @DeleteMapping("/{id}")
-    public String deleteUser(@PathVariable("id") long id) {
-        userService.delete(id);
-        return "redirect:/admin";
-    }
+//    @GetMapping
+//    public String main(Model model, Principal principal) {
+//        model.addAttribute("user", userService.findByUsername(principal.getName()));
+//        model.addAttribute("users",userService.getAll());
+//        model.addAttribute("listRoles", userService.listRoles());
+//        return "admin";
+//    }
+//
+//    @GetMapping("/search")
+//    public String searchUser(@RequestParam(value = "keyword") String keyword, Model model) {
+//        model.addAttribute("users", userService.search(keyword));
+//        return "searchUser";
+//    }
+//
+//    @GetMapping("/newUser")
+//    public String newUser(Model model) {
+//        model.addAttribute("user", new User());
+//        model.addAttribute("listRoles", userService.listRoles());
+//        return "newUser";
+//    }
+//
+//    @PostMapping()
+//    public String createUser(@ModelAttribute("user") User user) {
+//        userService.save(user);
+//        return "redirect:/admin";
+//    }
+//
+//    @GetMapping("/{id}/edit")
+//    public String editUser(@PathVariable("id") long id, Model model) {
+//        model.addAttribute("user", userService.get(id));
+//        model.addAttribute("listRoles", userService.listRoles());
+//        return "editUser";
+//    }
+//
+//    @PatchMapping("/{id}")
+//    public String updateUser(@ModelAttribute("user") User user, @PathVariable("id") long id) {
+//        userService.update(user);
+//        return "redirect:/admin";
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public String deleteUser(@PathVariable("id") long id) {
+//        userService.delete(id);
+//        return "redirect:/admin";
+//    }
 
 }
