e00f766e0eb88432d2afac27943d4b0426ee7dd8
Home <79774488441@ya.ru>
2022-09-10 02:03:14 +0300

    edit start

---
+++ src/main/resources/static/js/fetch/editExample.js
@@ -0,0 +1,121 @@
+function modalEdit(id) {
+
+    fetch('http://localhost:8080/admin/users/' + id)
+        .then(response => response.json())
+        .then(user => {
+
+            let adminSelect = "";
+            let userSelect = "";
+
+            for (let i = 0; i < user.userRoles.length; i++) {
+                if (user.userRoles[i].roleName == "ROLE_ADMIN") {
+                    adminSelect = "selected";
+                }
+                if (user.userRoles[i].roleName == "ROLE_USER") {
+                    userSelect = "selected";
+                }
+            }
+
+            let modal = document.getElementById('modalWindow');
+
+            modal.innerHTML =
+                '<div id="modalEdit"' +
+                '     class="modal fade" tabindex="-1" role="dialog"' +
+                '     aria-labelledby="TitleModalLabel" aria-hidden="true"' +
+                '     data-backdrop="static" data-keyboard="false">' +
+                '    <div class="modal-dialog modal-dialog-scrollable">' +
+                '        <div class="modal-content">' +
+                '            <div class="modal-header">' +
+                '                <h5 class="modal-title" id="TitleModalLabel">Edit user</h5>' +
+                '                <button type="button" class="close" data-dismiss="modal" aria-label="Close">' +
+                '                    <span aria-hidden="true">&times;</span>' +
+                '                </button>' +
+                '            </div>' +
+                '            <div class="modal-body bg-white">' +
+                '                <form id="formEditUser" style="width: 200px;"' +
+                '                       class="form-signin mx-auto font-weight-bold text-center">' +
+                '                    <p>' +
+                '                        <label>ID</label>' +
+                '                        <input class="form-control form-control-sm" type="text"' +
+                '                               id="editID" name="id" value="' + user.id + '" readonly>' +
+                '                    </p>' +
+                '                     <p>' +
+                '                        <label>Username</label>' +
+                '                        <input class="form-control form-control-sm" type="text"' +
+                '                               value="' + user.username + '" readonly>' +
+                '                    </p>' +
+                '                    <p>' +
+                '                        <label>Email</label>' +
+                '                        <input class="form-control form-control-sm" type="email"' +
+                '                               value="' + user.email + '" readonly>' +
+                '                    </p>' +
+                '                    <p>' +
+                '                        <label>Role</label>' +
+                '                        <select id="editRoles" name="roles" multiple size="2" required="required" ' +
+                '                               class="form-control form-control-sm">' +
+                '                            <option value="ROLE_ADMIN"' + adminSelect + '>ADMIN</option>' +
+                '                            <option value="ROLE_USER"' + userSelect + '>USER</option>' +
+                '                        </select>' +
+                '                    </p>' +
+                '                </form>' +
+                '            </div>' +
+                '            <div class="modal-footer">' +
+                '                <button type="button" class="btn btn-secondary"' +
+                '                        data-dismiss="modal">Close</button>' +
+                '                <button class="btn btn-primary" data-dismiss="modal"' +
+                '                        onclick="editUser()">Edit</button>' +
+                '            </div>' +
+                '        </div>' +
+                '    </div>' +
+                '</div>';
+
+            $("#modalEdit").modal();
+
+        });
+}
+
+function editUser() {
+
+    let form = window.formEditUser.editRoles;
+    let new_Roles = [];
+    let rolesList = document.createElement('ul');
+
+    for (var i = 0; i < form.length; i++) {
+        var option = form.options[i];
+        let role = document.createElement('li');
+        if (option.selected) {
+            new_Roles[i] = option.value
+
+            role.textContent = option.value.substring(5, 10) + " ";
+            rolesList.appendChild(role);
+        }
+    }
+
+    let id = window.formEditUser.editID.value;
+
+    fetch('http://localhost:8080/admin/users/' + id, {
+        method: 'PUT',
+        body: JSON.stringify({
+            id: window.formEditUser.editID.value,
+            name: window.formEditUser.editName.value,
+            lastName: window.formEditUser.editLastName.value,
+            age: window.formEditUser.editAge.value,
+            username: window.formEditUser.editUsername.value,
+            password: window.formEditUser.editPassword.value,
+            userRoles: new_Roles
+        }),
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+        .then(response => {
+            $('#' + id).replaceWith('<tr id=' + id + '>' +
+                '<td>' + id + '</td>' +
+                '<td>' + window.formEditUser.editName.value + '</td>' +
+                '<td>' + window.formEditUser.editLastName.value + '</td>' +
+                '<td>' + window.formEditUser.editAge.value + '</td>' +
+                '<td>' + window.formEditUser.editUsername.value + '</td>' +
+                '<td>' + rolesList.textContent + '</td>' +
+                '<td> <button type="button" onclick="modalEdit(' + id + ')" class="btn btn-primary btn-sm">Edit</button> </td>' +
+                '<td> <button type="button" onclick="modalDelete(' + id + ')" class="btn btn-danger btn-sm">Delete</button> </td>' +
+                '</tr>');
+        });
+}
